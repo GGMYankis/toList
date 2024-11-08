51a0406ea17a1a3c9042b7b726f5355266d7151b
genaogit <giancarlosGenaoMora14@gmail.com>
2024-11-08 09:19:43 -0400

    conexion

--- src/controller/login/database/conection.ts
+++ src/controller/login/database/conection.ts
@@ -1,11 +1,29 @@
 import * as sql from 'mssql';
 
 
-export const sqlConfig: sql.config = {
-    user: 'sa', // Usuario de tu base de datos
-    password: 'developer12x#', // Contraseña de tu base de datos
+/* export const sqlConfig: sql.config = {
+    user: 'sa', 
+    password: 'developer12x#', 
     database: 'taskMaster',
-    server: 'DEVELOPER', // Cambia esto si no estás en un servidor local
+    server: 'DEVELOPER', 
+    options: {
+        encrypt: false, 
+        trustServerCertificate: true,
+        enableArithAbort: false
+    },
+    pool: {
+        max: 100,
+        min: 0,
+        idleTimeoutMillis: 300000,
+    },
+};
+ */
+
+export const sqlConfig: sql.config = {
+    user: 'db_aaabc4_enfasis_admin', // Usuario de tu base de datos
+    password: 'enfasisdb2004', // Contraseña de tu base de datos
+    database: 'db_aaabc4_enfasis',
+    server: 'SQL8005.site4now.net', // Cambia esto si no estás en un servidor local
     options: {
         encrypt: false, // Si no usas conexiones seguras (TLS/SSL), deja esto en false
         trustServerCertificate: true,
@@ -17,7 +35,6 @@ export const sqlConfig: sql.config = {
         idleTimeoutMillis: 300000,
     },
 };
-
 export async function connectToDatabase() {
     try {
         return await sql.connect(sqlConfig);
